e3808918c0d33636ce1c5d2a414cb701676ad053
RostilKant <rostilnytskiy@gmail.com>
2021-01-18 19:36:04 +0200

    Adding game service

--- src/app/pages/guest-login/guest-login.page.ts
+++ src/app/pages/guest-login/guest-login.page.ts
@@ -3,6 +3,9 @@ import { FormControl, Validators } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Router } from '@angular/router';
 
+// services
+import { GameService } from '../../services/game.service';
+
 @UntilDestroy()
 @Component({
   selector: 'app-guest-login',
@@ -12,12 +15,12 @@ import { Router } from '@angular/router';
 export class GuestLoginPage {
   public name = new FormControl('', [Validators.required]);
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private gameService: GameService) {}
 
   public connectGame(): void {
     console.log('called create user');
-    // this.gameService.connectGame(this.name.value).pipe(untilDestroyed(this)).subscribe(() => {
-    //   this.router.navigate(['game']);
-    // });
+    this.gameService.connectGame(this.name.value).pipe(untilDestroyed(this)).subscribe(() => {
+      this.router.navigate(['game']);
+    });
   }
 }
